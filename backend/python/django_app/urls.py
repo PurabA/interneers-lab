f7af20d13072a840143ddba4aeeb8c88955af80f
Purab Agarwal <purab1275@gmail.com>
2026-02-14 17:35:49 +0530

    created api to greet based on name query parameter

--- backend/python/django_app/urls.py
+++ backend/python/django_app/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def hello_world(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server.")
+    # return HttpResponse("Hello, world! This is Rippling's interneers-lab Django server.")
+    name = request.GET.get('name', 'world')
+    return JsonResponse({"message": f"Hello, {name}! This is Rippling's interneers-lab Django server!"})
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
